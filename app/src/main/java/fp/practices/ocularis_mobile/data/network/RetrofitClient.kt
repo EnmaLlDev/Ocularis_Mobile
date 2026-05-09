803aac157c59d20e912c8c77d393f0f5a5a5c6c9
enmanuel.lledo <enmanuel.lledo@SLUG>
2026-05-09 17:27:01 +0200

    Corrgido bug login

--- app/src/main/java/fp/practices/ocularis_mobile/data/network/RetrofitClient.kt
+++ app/src/main/java/fp/practices/ocularis_mobile/data/network/RetrofitClient.kt
@@ -9,7 +9,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    // Usa la IP local de tu PC (obtenida con ipconfig)
+    // Ejemplo: private const val BASE_URL = "http://192.168.1.50:8080/"
+    private const val BASE_URL = "http://192.168.1.128:8080/"
 
     @Volatile
     private var initialized = false
